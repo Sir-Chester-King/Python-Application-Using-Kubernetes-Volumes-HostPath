1025c92d0ac3534c54a11678a1d31836b7d88d25
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-08-29 10:14:30 +0200

    code updated

---
+++ Classes/user_class.py
@@ -0,0 +1,39 @@
+# Class for the User objects instances.
+# The purpose of ths class is to set and get user's personal info.
+class User(object):
+
+    # Declare the constructor function.
+    # It's the initialize function called when a new object instance in created
+    def __init__(self, name, surname, address, phone_number):
+        # Here declared the attributes of the instances
+        # The keyword "self" indicate the object instance, the other are the parameters send via function "constructor"
+        self.name = name
+        self.surname = surname
+        self.address = address
+        self.phone_number = phone_number
+
+    # SET Functions ------------- #
+    def set_name(self, name):
+        self.name = name
+
+    def set_surname(self, surname):
+        self.surname = surname
+
+    def set_address(self, address):
+        self.address = address
+
+    def set_phone_number(self, phone_number):
+        self.phone_number = phone_number
+
+    # GET Functions ------------- #
+    def get_name(self):
+        return self.name
+
+    def get_surname(self):
+        return self.surname
+
+    def get_address(self):
+        return self.address
+
+    def get_phone_number(self):
+        return self.phone_number
--- Main_Code/create_data.py
+++ Main_Code/create_data.py
@@ -1,18 +1,20 @@
 # Import modules section
+import pickle
 import re
 
+from Classes.user_class import User
 from clean_console import *
 
 
-# Function to create a new User.
-def create_user():
-    clear()
+# Function to gather the user's input.
+def ask_data():
 
     # Insert data section.
     print("Define here the new user.", end="\n")
     name = input("Name: ")
     surname = input("Surname: ")
     address = input("Address: ")
+
     while True:
         phone_number = input("Phone number: ")
 
@@ -20,22 +22,14 @@ def create_user():
         if re.match(r"^[-+]?\d{2,15}$", phone_number):
             break
 
-    while True:
-        print("Do you want to check data to be sure that's all correct?")
-        check = input("Yes or No (y / n): ").lower()
-        if check == "y":
-            check_input(name, surname, address, phone_number)
-        elif check == "n":
-            break
-
-    """# Creating a new instance "USER" to pass ad parameter to the proper function to be stored in a file.
-    user = Classes.Users.User(name, surname, address, phone_number)
-
-    # Function to store the new user in a file.
-    Store_Data.Store_data.Store_Data_Into_Volume(user)"""
+    return name, surname, address, phone_number
 
 
+# Check the validate input
 def check_input(name, surname, address, phone_number):
+    # False as default for not correct info inserted.
+    check_value = False
+
     print("\n")
     print("Here the info's defined.")
     print("Name: " + name)
@@ -47,4 +41,114 @@ def check_input(name, surname, address, phone_number):
     while True:
         correct = input("Yes or No (y / n): ").lower()
         if correct == "y":
-            break
+            check_value = True  # All info are correct.
+            break  # Break the loop.
+
+        elif correct == "n":
+            break  # Break the loop, without changing the check_value variable (FALSE as default).
+
+    if check_value:
+        return True  # Input ARE correct.
+    else:
+        return False  # Input are NOT correct.
+
+
+# Function to check if the "Storage" directory exist or not.
+# If not, it will be created.
+def storage_directory():
+    # This is the PATH inside the Project Directory (current directory)
+    absolute_path = os.path.abspath(__file__)
+
+    # Go up one level
+    one_level_up = os.path.dirname(absolute_path)
+
+    # Go up two levels
+    two_level_up = os.path.dirname(one_level_up)
+
+    # Path of the "Storage" directory.
+    directory_storage = os.path.join(two_level_up, "Storage")
+
+    # Check if the directory inside the project exist or not.
+    # In case it doesn't exist, it is created.
+    if not os.path.exists(directory_storage):
+        os.makedirs(directory_storage)
+        print(f"Created directory: {directory_storage}")
+
+    return directory_storage
+
+
+# Function to create a new User.
+def create_user():
+    name, surname, address, phone_number = None, None, None, None
+
+    # False as default for not correct info inserted.
+    correct_user_insert = False
+
+    while not correct_user_insert:
+        clear()
+        name, surname, address, phone_number = ask_data()
+
+        while True:
+            print("Do you want to check data to be sure that's all correct?")
+            check = input("Yes or No (y / n): ").lower()
+
+            if check == "y":
+                check_value = check_input(name, surname, address, phone_number)
+
+                # Input ARE correct.
+                if check_value:
+                    correct_user_insert = True
+                    break  # Break the loop.
+                else:
+                    break  # Break the loop, without changing the correct_user_insert variable (FALSE as default).
+
+            elif check == "n":
+                correct_user_insert = True
+                break
+
+    # Creating a new instance "USER" to pass ad parameter to the proper function to be stored in a file.
+    new_user_instance = User(name, surname, address, phone_number)
+
+    # Check or create the "Storage" directory.
+    directory_storage = storage_directory()
+
+    # Name of the file will contain the user's data.
+    # Using PICKLE Serializing.
+    file_name_storage = "Users_Data.pkl"
+
+    # Path of the txt file where the user's data will stored
+    file_path = os.path.join(directory_storage, file_name_storage)
+
+    # Writing the data in the file.
+    try:
+        # Store the object in a file using pickle.
+        # Open the file in binary write mode.
+        with open(file_path, "wb") as file:
+
+            # Serialize (pickle) the object and save it to the file.
+            pickle.dump(new_user_instance, file)
+    except FileNotFoundError:
+        # Store the object in a file using pickle.
+        # Open the file in binary write mode.
+        with open(file_path, "wb") as file:
+
+            # Serialize (pickle) the object and save it to the file.
+            pickle.dump(new_user_instance, file)
+    except PermissionError:
+        print("You do not have permission to access this file.")
+    except IOError:
+        print("An I/O error occurred while writing the file.")
+    except pickle.UnpicklingError as e:
+        print(f"An unexpected error occurred: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+    # Open the file in binary read mode.
+    with open(file_path, 'rb') as file:
+
+        # Deserialize (unpickle) the object from the file
+        loaded_data = pickle.load(file)
+    print("Read", loaded_data)
+
+
+create_user()
