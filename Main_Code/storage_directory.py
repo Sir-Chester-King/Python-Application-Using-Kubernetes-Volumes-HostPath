df84d728867460e0ce7f3f7d859f0e8a497a6039
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-09-07 15:39:50 +0200

    Updated code.

--- Main_Code/storage_directory.py
+++ Main_Code/storage_directory.py
@@ -1,12 +1,13 @@
 # Import modules section.
 import os
+from pathlib import Path
 
 
 # Function to check if the "Storage" directory exist or not.
 # If not, it will be created.
 def storage_directory():
-    path_storage = os.getenv("Path_Workdir")  # Environment variable gather form Dockerfile.
-    name_file_storage = os.getenv("Name_File_Storage")  # Environment variable gather form Dockerfile.
+    path_storage = Path(os.getenv("Path_Storage"))  # Environment variable gather form Dockerfile.
+    name_file_storage = Path(os.getenv("Name_File_Storage"))  # Environment variable gather form Dockerfile.
 
     # Check if the directory inside the project exists or not.
     # In case it doesn't exist, it is created.
@@ -15,6 +16,6 @@ def storage_directory():
         print(f"Created directory: {path_storage}")
 
     # Path of the "Storage" directory.
-    directory_container_storage = os.path.join(path_storage, name_file_storage)
+    directory_container_storage = path_storage / name_file_storage
 
     return directory_container_storage
